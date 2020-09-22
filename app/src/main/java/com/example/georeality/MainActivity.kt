57488e63d35ebad9e8483a3090d1bbd6322e8087
Topias Peiponen <topiaspe@metropolia.fi>
2020-09-22 15:27:00 +0300

    drawer navigation

--- app/src/main/java/com/example/georeality/MainActivity.kt
+++ app/src/main/java/com/example/georeality/MainActivity.kt
@@ -2,12 +2,46 @@ package com.example.georeality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+/**
+ * @author Topias Peiponen, Roope Vaarama
+ * @since 22.09.2020
+ */
+
+/**
+ * MainActivity is the base activity, which contains the navigation and map functionality
+ */
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setupNav()
 
+    }
+
+    /**
+     * Set up the top app bar and related navigation
+     */
+    private fun setupNav() {
+        setSupportActionBar(topAppBar)
+
+        val drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            topAppBar,
+            R.string.app_name,
+            R.string.app_name
+        )
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        navigation.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return false
     }
 }
