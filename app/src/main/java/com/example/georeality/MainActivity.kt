48312a708f78dd946d4862cf5e29640724a97ec7
Topias Peiponen <topiaspe@metropolia.fi>
2020-09-24 14:29:00 +0300

    Layout changed, map transferred to own map fragment

--- app/src/main/java/com/example/georeality/MainActivity.kt
+++ app/src/main/java/com/example/georeality/MainActivity.kt
@@ -6,23 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.GoogleMap
 import android.Manifest
-import android.annotation.SuppressLint
-import android.location.Location
 import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.maps.OnMapReadyCallback
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.FragmentTransaction
 import com.firebase.ui.auth.AuthUI
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -36,22 +25,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 /**
  * MainActivity is the base activity, which contains the top navigation and map functionality
  */
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var mAuth : FirebaseAuth
-    private lateinit var map: GoogleMap
     private lateinit var providers : List<AuthUI.IdpConfig>
+    private lateinit var fragmentTransaction : FragmentTransaction
     private var user : FirebaseUser? = null
     private val REQUEST_CODE = 1
-    private  lateinit var lastLocation: Location
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fab.setOnClickListener(fabClickListener)
 
         //Check if user is logged in, if not then go to login screen
         mAuth = FirebaseAuth.getInstance()
@@ -61,21 +46,49 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             Log.d("User", user!!.email!!)
             setupNav()
-
-            val mapFragment =
-                supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-            mapFragment?.getMapAsync(this)
+            checkPermissions()
+            switchToMapFragment()
         }
+    }
+
+    private fun checkPermissions() {
+        if ((Build.VERSION.SDK_INT >= 26 && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
+    }
+
+    private fun switchToMapFragment() {
+        fragmentTransaction = supportFragmentManager.beginTransaction()
+        val mapFragment = MapFragment()
+        fragmentTransaction.replace(R.id.fragment_container, mapFragment)
+        fragmentTransaction.commit()
+    }
+    private fun switchToUserCachesFragment() {
+
+    }
+    private fun switchToCreateCacheFragment() {
+
     }
 
     private fun userIsLoggedIn() : Boolean {
         return user != null
     }
+    fun isUserAnonymous() : Boolean {
+        return user!!.isAnonymous
+    }
+
 
     private fun showSignInOptions() {
         providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.AnonymousBuilder().build()
+            AuthUI.IdpConfig.EmailBuilder().build()
         )
         // Create and launch sign-in intent
         startActivityForResult(
@@ -94,7 +107,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun setupNav() {
         setSupportActionBar(topAppBar)
 
-        navigation.apply {  }
         val drawerToggle = ActionBarDrawerToggle(
             this,
             drawer_layout,
@@ -123,77 +135,4 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
      * ============================================
      */
 
-    override fun onMapReady(googleMap: GoogleMap?) {
-        map = googleMap ?: return
-        googleMap.setOnMyLocationButtonClickListener(this)
-        googleMap.setOnMyLocationClickListener(this)
-        enableMyLocation()
-        
-    }
-
-    //Enables My Location layer if the fine location permission has been granted.
-    private fun enableMyLocation() {
-        if (!::map.isInitialized) return
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED
-        ) {
-            map.isMyLocationEnabled = true
-        } else {
-            //Permission to access the location is missing. Show rationale and request permission
-            if ((Build.VERSION.SDK_INT >= 26 && ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED)
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    0
-                )
-            }
-        }
-        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-            if (location != null) {
-                lastLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-            }
-        }
-    }
-
-    //Fab listener for adding markers and entitys
-    @SuppressLint("MissingPermission")
-    private val fabClickListener = View.OnClickListener {
-        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-            if (location != null) {
-                lastLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-            }
-            Toast.makeText(
-                this,
-                "Current location:${lastLocation.latitude} ${lastLocation.longitude}",
-                Toast.LENGTH_LONG
-            ).show()
-
-
-            map.addMarker(
-                MarkerOptions()
-                    .position(LatLng(lastLocation.latitude, lastLocation.longitude))
-
-            )
-        }
-    }
-
-    override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_LONG).show()
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false
-    }
-
-    override fun onMyLocationClick(location: Location) {
-        Toast.makeText(this, "Current locatin:\n$location", Toast.LENGTH_LONG).show()
-
-    }
 }
