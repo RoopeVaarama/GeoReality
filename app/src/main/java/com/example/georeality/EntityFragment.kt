038948621c5c2758c7cd1ee4f79268268609c59f
RoopeVaarama <roope.vaarama@gmail.com>
2020-09-25 11:55:53 +0300

    sad

--- app/src/main/java/com/example/georeality/EntityFragment.kt
+++ app/src/main/java/com/example/georeality/EntityFragment.kt
@@ -1,10 +1,16 @@
 package com.example.georeality
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +40,22 @@ class EntityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entity, container, false)
+        val view = inflater.inflate(R.layout.fragment_entity, container, false)
+        val spinner: Spinner = view.findViewById(R.id.spinner)
+        spinner.onItemSelectedListener = SpinnerActivity()
+        //Crate an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            activity!!.applicationContext,
+            R.array.type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            //Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            //Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        return view
     }
 
     companion object {
@@ -55,5 +76,15 @@ class EntityFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+}
+
+class SpinnerActivity: Activity(), AdapterView.OnItemSelectedListener {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        parent?.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
