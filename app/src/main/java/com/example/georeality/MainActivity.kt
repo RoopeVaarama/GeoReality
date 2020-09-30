498a328fd159ee06b820d3a9eb69656fdacc75ce
RoopeVaarama <roope.vaarama@gmail.com>
2020-09-30 14:34:40 +0300

    sad

--- app/src/main/java/com/example/georeality/MainActivity.kt
+++ app/src/main/java/com/example/georeality/MainActivity.kt
@@ -15,6 +15,7 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
