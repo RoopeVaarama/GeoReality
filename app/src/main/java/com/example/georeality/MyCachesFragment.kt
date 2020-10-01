fd635918137bcd6c4e8106589f01dd8eee3b1d8f
RoopeVaarama <roope.vaarama@gmail.com>
2020-10-01 18:35:54 +0300

    asd

--- app/src/main/java/com/example/georeality/MyCachesFragment.kt
+++ app/src/main/java/com/example/georeality/MyCachesFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MyCachesFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var dataset = mutableListOf("Item 1", "Item 2", "Item 3")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
+        viewManager = LinearLayoutManager(requireActivity())
+        viewAdapter = MyRe
         return inflater.inflate(R.layout.fragment_my_caches, container, false)
     }
 }
