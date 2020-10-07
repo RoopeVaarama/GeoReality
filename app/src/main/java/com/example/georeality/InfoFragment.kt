75444e23d33246fee5c997c2488186c84284e4a5
TopiasPeiponen <topiaspeiponen@metropolia.fi>
2020-10-07 12:01:44 +0300

    info page implemented

--- app/src/main/java/com/example/georeality/InfoFragment.kt
+++ app/src/main/java/com/example/georeality/InfoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_info.*
 
 /**
  * @author Roope Vaarama, Topias Peiponen
@@ -22,5 +23,12 @@ class InfoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        info_fragment_red_img.setImageResource(R.drawable.google_maps_marker_red)
+        info_fragment_blue_img.setImageResource(R.drawable.google_maps_marker_blue)
+        info_fragment_infowindow_img.setImageResource(R.drawable.georeality_infowindow_example)
     }
 }
